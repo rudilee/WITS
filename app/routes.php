6e501529473a88e18c54055361a21f382db16681
Rudi Lee <quadhack@gmail.com>
2014-10-17 11:53:49 +0700

    Ngelengkapin fungsi muat tampilan login sama navigasi, dll

--- app/routes.php
+++ app/routes.php
@@ -11,17 +11,34 @@
 |
 */
 
-Route::get('/test', function()
-{
-	return View::make('home', array(
-		'unfinished' => 13,
-		'finished' => 2190,
-		'open' => 5,
-		'in_progress' => 8,
-		'resolved' => 1024
-	));
+Route::get('/api/user', function() {
+    return Response::json(array(
+        'username' => 'hehehe'
+    ), 401);
 });
 
-Route::get('/login', function() {
-	return View::make('login');
+Route::post('/api/user', function() {
+    return Response::json(array(
+        'username' => 'umbrus',
+        'message' => 'Username atau Password anda salah.'
+    ), 200);
+});
+
+Route::delete('/api/user', function() {
+    return Response::json(array(
+        'hehehe' => 'hihihi'
+    ), 200);
+});
+
+Route::post('/api/search', function() {
+    return Response::json(array(
+        'result' => array(
+            'Hasil pertama',
+            'Cuman ngetest doank',
+            'Yah gitu deh hahaha',
+            'Mleketek jum jum wau wau',
+            'Ini hasil pencarian yg ke-5'
+        ),
+        'query' => strip_tags(Input::get('query'))
+    ), 200);
 });
--- public/js/main.js
+++ public/js/main.js
@@ -12,21 +12,21 @@ jQuery(document).ready(function ($) {
     
     // Cache hasil getScript()
     $.ajaxSetup({
-        cache: true
+         cache: false
     });
     
     // Muat menu bar navigasi
-    function loadNavigation() {
+    function loadNavigation(username) {
         $('#navbar-collapse').load('templates/navigation.html', function () {
             $('.badge').tooltip();
-
+            
             // Routing link2 di menu bar dg nangkep semua link yg diklik terus cek isi hash-nya
             $('#navbar-collapse a').click(function (event) {
                 if (this.hash !== '') {
                     var route = this.hash.replace('#', '');
 
                     // Cek apa ada di daftar routing
-                    if (typeof routes[route] !== 'undefined') {
+                    if ($.inArray(route, routes) > -1) {
                         // Kalo objek sub-modul belum ada muat script-nya terus render
                         if (typeof wits[route] === 'undefined') {
                             $.getScript('js/' + route + '.js', function (script, textStatus, jqXHR) {
@@ -38,31 +38,82 @@ jQuery(document).ready(function ($) {
                     }
                 }
             });
+            
+            // Cari Work atau Issue berdasar isi judul & deskripsi, atau dari id-nya
+            $('#search').submit(function(event) {
+                var query = $('#query').val();
+                
+                if (query !== '') {
+                    $('#content').load('templates/search.html', function() {
+                        $.post('api/search', {
+                            query: query
+                        }).done(function(data, textStatus, jqXHR) {
+                            var resultList = $('#result');
+
+                            $('#search-query').text(data.query);
+
+                            // Iterasi hasil pencarian, tampilin di semacem list-view gitu
+                            $.each(data.result, function(indexInArray, value) {
+                                resultList.append('<li>' + value + '</li>');
+                            });
+                        });
+                    });
+                }
+                
+                event.preventDefault();
+            });
+            
+            // Tampilin username di option user
+            $('#name').text(username);
+
+            // Logout dari aplikasi
+            $('#logout').click(function() {
+                $.ajax({
+                    url: 'api/user',
+                    type: 'DELETE'
+                }).done(function(data, textStatus, jqXHR) {
+                    loadLoginForm();
+                });
+            });
         });
     }
     
     // Muat form login
     function loadLoginForm() {
         $('#content').load('templates/login.html', function () {
+            $('#navbar-collapse').html('');
+            
             // TODO: kalo berhasil login tampilin menu navigasi
+            $('#login').submit(function(event) {
+                $.post('api/user', {
+                    username: $('#username').val(),
+                    password: $('password').val()
+                }).done(function(data, textStatus, jqXHR) {
+                    loadNavigation(data.username);
+                    
+                    $('#content').html('<h1>Login Berhasil</h1>');
+                }).fail(function(jqXHR, textStatus, errorThrown) {
+                    if (errorThrown === 'Not Acceptable')
+                        alert(JSON.parse(jqXHR.responseText).message);
+                });
+                
+                event.preventDefault();
+            });
         });
     }
     
     // Kalo request ajax gagal karna belum login(Unauthorized) tampilin form login
     $(document).ajaxError(function (event, jqXHR, ajaxSettings, thrownError) {
         if (thrownError === 'Unauthorized') {
-            $('#navbar-collapse').html('');
-            
             loadLoginForm();
         } else {
             // TODO: kalo error laen, tampilin messageBox kali?
+            console.log('Result: ' + thrownError);
         }
     });
     
     // Cek sudah login atau belum
-    $.get('/user', function (data, textStatus, jqXHR) {
-        loadNavigation();
-    }, 'json').fail(function (jqXHR, textStatus, errorThrown) {
-        loadLoginForm();
-    });
+    $.get('api/user', function (data, textStatus, jqXHR) {
+        loadNavigation(data.username);
+    }, 'json');
 });
